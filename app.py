592d1ad759665434ff63061c3020eeac90e660a2
am-pythoncoder700 <adityamahato8924@gmail.com>
2024-12-10 16:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,6 @@ import pickle
 from flask import Flask, render_template, request
 import numpy as np
 
-# Implementing our Simple Linear Regression class
-class Simple_Linear_Regression:
-    def __init__(self, coef_=None, intercept_=None):
-        self.coef_ = coef_
-        self.intercept_= intercept_
-    def fit(self, X_train, y_train):
-        x_mean, y_mean = np.mean(X_train), np.mean(y_train)
-        self.coef_ = np.dot((y_train - y_mean), (X_train - x_mean)) / np.dot((X_train - x_mean), (X_train - x_mean))
-        self.intercept_ = y_mean - self.coef_ * x_mean
-    def predict(self, X_test):
-        return self.coef_ * X_test + self.intercept_
-    def get_params(self, deep):
-        return {"coef_" : self.coef_, "intercept_" : self.intercept_}
-
 app = Flask(__name__)
 
 # Mock Model for Prediction
@@ -32,8 +18,8 @@ def index():
 def predict():
     try:
         cgpa = float(request.form['cgpa'])
-        prediction = predict_package(cgpa)
-        return render_template('index.html', prediction=f"{prediction:.2f}")
+        prediction = predict_package([[cgpa])
+        return render_template('index.html', prediction=f"{prediction[0]:.2f}")
     except ValueError:
         return render_template('index.html', prediction="Invalid Input")
 
